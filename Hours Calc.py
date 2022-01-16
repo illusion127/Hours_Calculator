445d3b37b1d334bc9f282e0c131c83b162cb1b5a
Sam <97695203+illusion127@users.noreply.github.com>
2022-01-16 18:47:26 +0000

    initial comit

---
+++ Hours Calc.py
@@ -0,0 +1,41 @@
+#Hours Calc
+import math
+loop1 = True
+while loop1:
+    print("Hello")
+    print("This is the hours calculator")
+
+    total = 0
+
+    loop = True
+    while loop:
+        A = int(input("how many days did they work?: "))
+        for i in range(1,A+1):
+            startinp1 = float(input("What time did they start on day " + str(i) + "?: "))
+            finishinp1 = float(input("What time did they finish on day " + str(i) + "?: "))
+
+            startmins = (startinp1 - (math.floor(startinp1))) * 100
+            starthours = math.floor(startinp1)
+            allstartmins = (starthours * 60) + startmins
+            finishmins = (finishinp1 - (math.floor(finishinp1))) * 100
+            finishhours = math.floor(finishinp1)
+            allfinishmins = (finishhours * 60) + finishmins
+            diff = allfinishmins - allstartmins
+
+            if diff > 360:
+                diff = diff - 20
+
+            diff = (diff / 60)
+            diff = round(diff, 2)
+            total = diff + total
+        print("they worked",total,"hours")
+        repeat = int(input("if you want to use this program again please type '0':"))
+        if repeat == 0:
+            A = 0
+            total = 0
+            loop = True
+        else:
+            loop = False
+            loop1 = False
+
+# Made And Designed By Samuel Ol
